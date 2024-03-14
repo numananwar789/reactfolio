cc474da4149aaaf921a71c7b5d713a6a43aec491
Numan Anwar <86041286+numananwar789@users.noreply.github.com>
2024-03-14 13:10:53 +0500

    first commit

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"Full-stack web designer and developer.",
+		keywords: ["Numan", "Numan Anwar", "Anwar"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I’m Numan Anwar, coding the future from Pakistan's tech hub.",
+		keywords: ["Numan", "Numan Anwar", "Anwar"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+		keywords: ["Numan", "Numan Anwar", "Anwar"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+		keywords: ["Numan", "Numan Anwar", "Anwar"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Numan", "Numan Anwar", "Anwar"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,93 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Numan Anwar",
+		name: "Numan Anwar",
+		email: "numananwar789@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/@call_me_numan",
+		github: "https://github.com/numananwar789",
+		linkedin: "https://linkedin.com/in/numan-anwar",
+		instagram: "https://instagram.com/call_me_numan_khan",
+		stackoverflow: "https://stackoverflow.com/users/20562044/numan-anwar",
+		facebook: "https://www.facebook.com/KhAn.NuMaN.789/",
+	},
+
+	homepage: {
+		title: "Full-stack web designer and developer.",
+		description:
+			"I am a web developer with expertise in HTML, CSS, PHP, Laravel & React JS. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a web developer.",
+	},
+
+	about: {
+		title: "I’m Numan Anwar, coding the future from Pakistan's tech hub.",
+		description:
+			"I've worked on a variety of projects over 1+ year and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "MDMS",
+			description:
+				"Medicine Distribution Management System. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.worldvectorlogo.com/logos/laravel-2.svg",
+			linkText: "View Project",
+			link: "https://github.com/numananwar789/mdms",
+		},
+
+		{
+			title: "Digital Clinic",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.worldvectorlogo.com/logos/laravel-2.svg",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Its Real",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.worldvectorlogo.com/logos/laravel-2.svg",
+			linkText: "View Project",
+			link: "https://github.com/numananwar789/itsreal.com.au",
+		},
+
+		{
+			title: "E Bazarr",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.worldvectorlogo.com/logos/laravel-2.svg",
+			linkText: "View Project",
+			link: "https://github.com/numananwar789/e-bazarr.com",
+		},
+
+		{
+			title: "Ralitnytrh",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.worldvectorlogo.com/logos/laravel-2.svg",
+			linkText: "View Project",
+			link: "https://github.com/numananwar789/realitnytrh.eu",
+		},
+
+		{
+			title: "Flick",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.worldvectorlogo.com/logos/laravel-2.svg",
+			linkText: "View Project",
+			link: "https://github.com/numananwar789/flick",
+		},
+	],
+};
+
+export default INFO;
